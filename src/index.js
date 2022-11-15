793d338819c464a938ca81d69d74c41b61ffbbb6
abreham1222 <abreham1222@gmail.com>
2022-11-15 23:20:49 +0300

    Add  importing js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log('h');
+import './style.css';
+import add from './add.js';
+
+add();
+const refrsh = document.querySelector('.refresh');
+refrsh.addEventListener('click', () => {
+  window.location.reload();
+});
