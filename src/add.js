10150cf3e484fd2922b9ff89726ed384ec982c93
abreham1222 <abreham1222@gmail.com>
2022-11-17 21:45:54 +0300

    Add function of display

--- src/add.js
+++ src/add.js
@@ -1,1 +1,17 @@
 import * as gameAPI from './gameAPI.js';
+
+const display = () => {
+  const show = document.querySelector('.show');
+  show.innerHTML = '';
+  gameAPI.get().then((dataList) => {
+    if (!dataList) {
+      return;
+    }
+    const high = dataList.result.sort((a, b) => b.score - a.score);
+    high.forEach((data) => {
+      const li = document.createElement('li');
+      li.innerHTML = `<p>${data.user}: ${data.score}</p>`;
+      show.appendChild(li);
+    });
+  });
+};
